16cdd9c1a5e58c61215522336e3bee49dfd2661b
Gojirarara <corawang@sas.upenn.edu>
2020-04-24 02:06:25 -0400

    Updates

---
+++ precache-manifest.700af6d426223071bfee280273eb3f97.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152640d0b2cdbd98581d01c0d1bce106",
+    "url": "/discover/index.html"
+  },
+  {
+    "revision": "beb9dd63a033a90e0207",
+    "url": "/discover/static/css/2.7a75a24e.chunk.css"
+  },
+  {
+    "revision": "8d4a6dabc7fae60cd2c6",
+    "url": "/discover/static/css/main.2aa59404.chunk.css"
+  },
+  {
+    "revision": "beb9dd63a033a90e0207",
+    "url": "/discover/static/js/2.79b561e4.chunk.js"
+  },
+  {
+    "revision": "7e08df03892269e6e39da8a34916b217",
+    "url": "/discover/static/js/2.79b561e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4a6dabc7fae60cd2c6",
+    "url": "/discover/static/js/main.275a4baf.chunk.js"
+  },
+  {
+    "revision": "5cad35c1ef8a6027063a",
+    "url": "/discover/static/js/runtime-main.d800e714.js"
+  },
+  {
+    "revision": "181ef3462c28b9ee88dacea5d9fd8bfd",
+    "url": "/discover/static/media/0.181ef346.png"
+  },
+  {
+    "revision": "b1f04b4268474d77cbdc93e616b2b4d1",
+    "url": "/discover/static/media/1.b1f04b42.png"
+  },
+  {
+    "revision": "b9fdb4b78b4ececea3441ccd593705e5",
+    "url": "/discover/static/media/2.b9fdb4b7.png"
+  },
+  {
+    "revision": "2147f2150da22fe5fef81de3d201d060",
+    "url": "/discover/static/media/4.2147f215.png"
+  },
+  {
+    "revision": "8b352b589cf690c32e99b055e13ba4ed",
+    "url": "/discover/static/media/7.8b352b58.png"
+  },
+  {
+    "revision": "7f3c04e4f738f42dec61074965df63f8",
+    "url": "/discover/static/media/DSL_Logo.7f3c04e4.png"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/discover/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/discover/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "f6238deb7f40a7a03134c11fb63ad387",
+    "url": "/discover/static/media/OpenSans-Italic.f6238deb.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/discover/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "c147d1302b974387afd38590072e7294",
+    "url": "/discover/static/media/OpenSans-LightItalic.c147d130.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/discover/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/discover/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/discover/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "a64e5d47236476cf1897f5402088766d",
+    "url": "/discover/static/media/aaron.a64e5d47.png"
+  },
+  {
+    "revision": "ae6cf4632bca5e60aaca90659ea01443",
+    "url": "/discover/static/media/banner.ae6cf463.svg"
+  },
+  {
+    "revision": "df7c8de2f7f174bae6b89d764effb065",
+    "url": "/discover/static/media/bidisha.df7c8de2.png"
+  },
+  {
+    "revision": "37398961d22240288aa3df500998229e",
+    "url": "/discover/static/media/cora.37398961.jpg"
+  },
+  {
+    "revision": "808097fdd9261660a6e5ed0b4e1fe14d",
+    "url": "/discover/static/media/jackie.808097fd.png"
+  },
+  {
+    "revision": "536cfe84784335b4798f7b0b23e00a11",
+    "url": "/discover/static/media/stacy.536cfe84.png"
+  },
+  {
+    "revision": "3df3166e8d954f32cf23c60408a093fe",
+    "url": "/discover/static/media/team.3df3166e.png"
+  }
+]);
